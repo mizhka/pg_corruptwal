6e7582996c4c6f32018f320c948ada8027603c43
Mikhail Zhilin <mizhka@FreeBSD.org>
2021-04-20 16:28:26 +0300

    [fix] compilation warnings

--- pg_corruptwal.h
+++ pg_corruptwal.h
@@ -10,11 +10,11 @@
 
 #include <sys/types.h>
 
-#define	ALIGN(ptr)				((char *) ((uintptr_t) ptr & ~(uintptr_t) (PAGESIZE - 1)))
+#define	ALIGN(ptr)				((char *) ((uintptr_t) (ptr) & ~(uintptr_t) (PAGESIZE - 1)))
 #define BIT_CHECK(mask, bit)	(((mask) & ((int)1 << (bit))) != 0)
-#define BIT_CLEAR(mask, bit)	(mask &= ~((int)1 << (bit)))
-#define BIT_SET(mask, bit)		(mask |= ((int)1 << (bit)))
-#define	IS_UNALIGNED(ptr)		((uintptr_t) ptr & ~(uintptr_t) (PAGESIZE - 1))
+#define BIT_CLEAR(mask, bit)	((mask) &= ~((int)1 << (bit)))
+#define BIT_SET(mask, bit)		((mask) |= ((int)1 << (bit)))
+#define	IS_UNALIGNED(ptr)		((uintptr_t) (ptr) & ~(uintptr_t) (PAGESIZE - 1))
 #define	PAGESIZE				0x1000
 
 struct wal_buffers_info {
